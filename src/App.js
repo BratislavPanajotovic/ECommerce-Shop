cf3e61a173b6a783bd38194a9282bf7011cc7a5a
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-03-07 16:51:46 +0100

    home view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import ProductProvider from "./contexts/ProductContext";
 
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return (
+    <div className="overflow-hidden">
+      <Router>
+        <ProductProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+          </Routes>
+          <Sidebar />
+          <Footer />
+        </ProductProvider>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
 
 const Home = () => {
-  return <div>Homepage</div>;
+  const { products } = useContext(ProductContext);
+  console.log(products);
+
+  const filteredProducts = products.filter((item) => {
+    return (
+      item.category === "mens's clothing" ||
+      item.category === "women's clothing"
+    );
+  });
+
+  return (
+    <div>
+      <section>
+        <div className="container mx-auto">
+          <div>
+            {filteredProducts.map((product) => {
+              console.log(filteredProducts);
+              return <div key={product.id}> {product.title} </div>;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
